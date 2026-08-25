ebb605210e27567b3520f0f9e226b516453fa434
adminat233-new <adminat233-new@users.noreply.github.com>
2026-08-25 14:13:19 +0000

    Frontend: location history trail on map, history polyline loading

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'laptop-tracker-v13-nativeagents';
+const CACHE_NAME = 'laptop-tracker-v14-history';
 const LOCATION_INTERVAL = 10000;
 let locationTimer = null;
 let trackedDeviceId = null;
