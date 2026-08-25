d13b52ba1e7552dee3974654543cb891955682f0
adminat233-new <adminat233-new@users.noreply.github.com>
2026-08-25 06:09:28 +0000

    Downgrade to Prisma 6 for Node 18 compatibility

--- cloud-server.js
+++ cloud-server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const crypto = require('crypto');
-const { PrismaClient } = require('./generated/prisma');
+const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 const PORT = process.env.PORT || 9999;
